67b117bca7a5a283b0c3d43e58874f53952bd520
Rita Zhang <rita.z.zhang@gmail.com>
2021-04-15 14:15:31 -0700

    feat(externaldata) - add external data support
    Signed-off-by: Rita Zhang <rita.z.zhang@gmail.com>

--- vendor/github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/local/local.go
+++ vendor/github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/local/local.go
@@ -12,10 +12,13 @@ import (
 	"github.com/open-policy-agent/frameworks/constraint/pkg/types"
 	"github.com/open-policy-agent/opa/ast"
 	"github.com/open-policy-agent/opa/rego"
+	opatypes "github.com/open-policy-agent/opa/types"
 	"github.com/open-policy-agent/opa/storage"
 	"github.com/open-policy-agent/opa/storage/inmem"
 	"github.com/open-policy-agent/opa/topdown"
 	"github.com/pkg/errors"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 const moduleSetPrefix = "__modset_"
@@ -50,6 +53,7 @@ func New(args ...Arg) drivers.Driver {
 		compiler: ast.NewCompiler(),
 		modules:  make(map[string]*ast.Module),
 		storage:  inmem.New(),
+		externalDataSources: make(map[string]*unstructured.Unstructured),
 	}
 	for _, arg := range args {
 		arg(d)
@@ -65,9 +69,37 @@ type driver struct {
 	modules      map[string]*ast.Module
 	storage      storage.Store
 	traceEnabled bool
+	externalDataSources  map[string]*unstructured.Unstructured
 }
 
 func (d *driver) Init(ctx context.Context) error {
+	rego.RegisterBuiltin2(
+		&rego.Function{
+			Name:    "externaldata",
+			Decl:    opatypes.NewFunction(opatypes.Args(opatypes.S, opatypes.S), opatypes.A),
+			Memoize: true,
+		},
+		func(bctx rego.BuiltinContext, a, b *ast.Term) (*ast.Term, error) {
+			var ed, input, result string
+
+			if err := ast.As(a.Value, &ed); err != nil {
+				return nil, err
+			}
+			if err := ast.As(b.Value, &input); err != nil {
+				return nil, err
+			}
+
+			result = fmt.Sprintf("input: %v, external datasource len: %v", input, len(d.externalDataSources))
+
+			if data, ok := d.externalDataSources[ed]; !ok {
+				result = fmt.Sprintf("external datasource %v not found", ed)
+			} else {
+				result = fmt.Sprintf("%s | %s ", result, data.GetKind())
+			}
+
+			return ast.StringTerm(input), nil
+		},
+	)
 	return nil
 }
 
@@ -236,6 +268,13 @@ func parsePath(path string) ([]string, error) {
 		return nil, fmt.Errorf("Bad data path: %s", path)
 	}
 	return p, nil
+}
+func (d *driver) AddExternalData(ctx context.Context, key string, data *unstructured.Unstructured) error {
+	d.modulesMux.RLock()
+	defer d.modulesMux.RUnlock()
+
+	d.externalDataSources[key] = data
+	return nil
 }
 
 func (d *driver) PutData(ctx context.Context, path string, data interface{}) error {
